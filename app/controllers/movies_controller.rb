5b5b225e9e2a883d72b8f69f424a37b9cb02ea09
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2024-05-01 16:37:49 -0300

    Implement create and add description field

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,7 +8,14 @@ class MoviesController < ApplicationController
     @movie = Movie.find(id)
   end
 
-  def new
-    # default: render 'new' template
+  def create
+    puts params[:movie]
+    @movie = Movie.create!({
+      title: params[:movie][:title],
+      rating: params[:movie][:rating],
+      description: params[:movie][:description],
+      release_date: Date.new(params[:movie]["release_date(1i)"].to_i, params[:movie]["release_date(2i)"].to_i, params[:movie]["release_date(3i)"].to_i),
+    })
+    redirect_to movies_path
   end
 end
