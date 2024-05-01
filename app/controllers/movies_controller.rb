d9f75e2dc9f1b2e61192739c4a021876b6a75f9d
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2024-05-01 17:21:25 -0300

    Movie editing

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,6 +8,22 @@ class MoviesController < ApplicationController
     @movie = Movie.find(id)
   end
 
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    @movie.update!({
+      title: params[:movie][:title],
+      rating: params[:movie][:rating],
+      description: params[:movie][:description],
+      release_date: Date.new(params[:movie]["release_date(1i)"].to_i, params[:movie]["release_date(2i)"].to_i, params[:movie]["release_date(3i)"].to_i),
+    })
+    flash[:notice] = "Movie '#{@movie.title}' was successfully updated."
+    redirect_to movies_path
+  end
+
   def create
     if params[:movie][:title].empty? || params[:movie][:rating].empty? || params[:movie][:description].empty? || params[:movie]["release_date(1i)"].empty? || params[:movie]["release_date(2i)"].empty? || params[:movie]["release_date(3i)"].empty?
       flash[:warning] = "Movie creation failed. Invalid data provided."
