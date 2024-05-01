f6040a41f331a943ec8ebabdc9be988d0ee56769
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2024-05-01 16:02:12 -0300

    Add movie view and form skeleton

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,13 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
+  def show
+    id = params[:id]
+    @movie = Movie.find(id)
+  end
+
+  def new
+    # default: render 'new' template
+  end
 end
