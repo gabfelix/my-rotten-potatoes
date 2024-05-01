f549898c313d2e0cff66ca1cecd1770b495edbe6
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2024-05-01 17:02:03 -0300

    Add messages

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,13 +9,18 @@ class MoviesController < ApplicationController
   end
 
   def create
-    puts params[:movie]
+    if params[:movie][:title].empty? || params[:movie][:rating].empty? || params[:movie][:description].empty? || params[:movie]["release_date(1i)"].empty? || params[:movie]["release_date(2i)"].empty? || params[:movie]["release_date(3i)"].empty?
+      throw :invalid_movie_input
+    end
+
+    # TODO: Throw if release_date is in the past
     @movie = Movie.create!({
       title: params[:movie][:title],
       rating: params[:movie][:rating],
       description: params[:movie][:description],
       release_date: Date.new(params[:movie]["release_date(1i)"].to_i, params[:movie]["release_date(2i)"].to_i, params[:movie]["release_date(3i)"].to_i),
     })
+    flash[:notice] = "Movie '#{@movie[:title]}' was successfully created."
     redirect_to movies_path
   end
 end
